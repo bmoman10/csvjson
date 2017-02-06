1e5777ba1c8dee5c5980f98465e1edf241890274
Blake <bmoman@stu.jsu.edu>
2017-02-05 20:45:30 -0600

    Adds csvToJson

--- src/main/java/edu/jsu/mcis/Converter.java
+++ src/main/java/edu/jsu/mcis/Converter.java
@@ -36,15 +36,136 @@ public class Converter {
             ]
         }  
     */
+	
+	private static String getColHeaders(List<String[]> lines) {
+		String colHeaders = "";
+		for(String[] token : lines) {
+			if(token == lines.get(0)) {
+				for(String element : token) {
+					if(element != token[0] && element != token[token.length-1]) {
+						colHeaders += "\"" + element + "\",";
+					}
+					if(element == token[token.length-1]) {
+						colHeaders += "\"" + element + "\"]" + ",\n"; 
+					}
+				}
+			}
+		}
+		return colHeaders;
+	}
+	
+	private static String getRowHeaders(List<String[]> lines) {
+		String rowHeaders = "";
+		for(String[] token : lines) {
+			if(token != lines.get(lines.size()-1) && token != lines.get(0)){
+				for(String element : token) {
+					if(element == token[0]) {
+						rowHeaders += "\"" + element + "\",";
+					}
+				}
+			}
+			else if(token == lines.get(lines.size()-1)) {
+				for(String element : token) {
+					if(element == token[0]) {
+						rowHeaders += "\"" + element + "\"]" + ",\n";
+					}
+				}
+			}
+		}
+		return rowHeaders;
+	}
+	
+	private static String getData(List<String[]> lines) {
+		String data = "";
+		for(String[] token : lines) {
+			if(token != lines.get(0) && token != lines.get(lines.size()-1)) {
+				for(String element : token) {
+					if(element == token[1]) {
+						data += "[" + element + ",";
+					}
+					else if(element != token[0] && element != token[token.length-1]) {
+						data += element + ",";
+					}
+					else if(element == token[token.length-1]) {
+						data += element + "]," + "\n" + "\t" + "\t";
+					}
+				}
+			}
+			if(token == lines.get(lines.size()-1)) {
+				for(String element : token) {
+					if(element == token[1]) {
+						data += "[" + element + ",";
+					}
+					else if(element != token[0] && element != token[token.length-1]) {
+						data += element + ",";
+					}
+					else if(element == token[token.length-1]) {
+						data += element + "]" + "\n" + "\t" + "]" + "\n" + "}";
+					}
+				}
+			}
+		}
+		return data;
+	}
+	
     
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
-        return "";
+		String jsonString = "{\n";
+		String colHeaders = "\t" + "\"colHeaders\":[";
+		String rowHeaders = "\t" + "\"rowHeaders\":[";
+		String data = "\t" + "\"data\":[";
+		CSVReader reader = new CSVReader(new StringReader(csvString));
+		try {
+			List<String[]> lines = reader.readAll();
+			colHeaders += getColHeaders(lines);
+			rowHeaders += getRowHeaders(lines);
+			data += getData(lines);
+		}
+		catch(IOException e) {}
+		jsonString += colHeaders;
+		jsonString += rowHeaders;
+		jsonString += data;
+        return jsonString;
     }
     
     public static String jsonToCsv(String jsonString) {
+		String csvString = "";
+		
+		try {
+			JSONParser parser = new JSONParser();
+			Object obj = parser.parse(jsonString);
+			JSONArray array = (JSONArray)obj;
+		} catch(Exception e) {}
+		
         return "";
     }
+	
+	public static void main(String[] args) {
+		String csvString = "\"ID\",\"Total\",\"Assignment 1\",\"Assignment 2\",\"Exam 1\"\n" +
+					"\"111278\",\"611\",\"146\",\"128\",\"337\"\n" +
+					"\"111352\",\"867\",\"227\",\"228\",\"412\"\n" +
+					"\"111373\",\"461\",\"96\",\"90\",\"275\"\n" +
+					"\"111305\",\"835\",\"220\",\"217\",\"398\"\n" +
+					"\"111399\",\"898\",\"226\",\"229\",\"443\"\n" +
+					"\"111160\",\"454\",\"77\",\"125\",\"252\"\n" +
+					"\"111276\",\"579\",\"130\",\"111\",\"338\"\n" +
+					"\"111241\",\"973\",\"236\",\"237\",\"500\"";
+		String jsonString = "{" + "\n" +
+			"\t" + "\"colHeaders\":[\"Total\",\"Assignment 1\",\"Assignment 2\",\"Exam 1\"],"  + "\n" + 
+			"\t" + "\"rowHeaders\":[\"111278\",\"111352\",\"111373\",\"111305\",\"111399\",\"111160\",\"111276\",\"111241\"]," + "\n" +
+			"\t" + "\"data\":[[611,146,128,337]," + "\n" +
+					"\t" + "\t" + "[867,227,228,412]," + "\n" +
+					"\t" + "\t" + "[461,96,90,275]," + "\n" +
+					"\t" + "\t" + "[835,220,217,398]," + "\n" +
+					"\t" + "\t" + "[898,226,229,443]," + "\n" + 
+					"\t" + "\t" + "[454,77,125,252]," + "\n" +
+					"\t" + "\t" + "[579,130,111,338]," + "\n" +
+					"\t" + "\t" + "[973,236,237,500]" + "\n" +
+				"\t" + "]" + "\n" +
+		"}";
+		System.out.println(jsonToCsv(jsonString));
+	}
 }
 
 
--- src/test/java/edu/jsu/mcis/ConverterTest.java
+++ src/test/java/edu/jsu/mcis/ConverterTest.java
@@ -11,22 +11,44 @@ import org.json.simple.parser.ParseException;
 public class ConverterTest {
     private String csvString;
     private String jsonString;
+	private Converter converter;
 
     @Before
     public void setUp() {
-
+		csvString = "\"ID\",\"Total\",\"Assignment 1\",\"Assignment 2\",\"Exam 1\"\n" +
+					"\"111278\",\"611\",\"146\",\"128\",\"337\"\n" +
+					"\"111352\",\"867\",\"227\",\"228\",\"412\"\n" +
+					"\"111373\",\"461\",\"96\",\"90\",\"275\"\n" +
+					"\"111305\",\"835\",\"220\",\"217\",\"398\"\n" +
+					"\"111399\",\"898\",\"226\",\"229\",\"443\"\n" +
+					"\"111160\",\"454\",\"77\",\"125\",\"252\"\n" +
+					"\"111276\",\"579\",\"130\",\"111\",\"338\"\n" +
+					"\"111241\",\"973\",\"236\",\"237\",\"500\"";
+		jsonString = "{" + "\n" +
+			"\t" + "\"colHeaders\":[\"Total\",\"Assignment 1\",\"Assignment 2\",\"Exam 1\"],"  + "\n" + 
+			"\t" + "\"rowHeaders\":[\"111278\",\"111352\",\"111373\",\"111305\",\"111399\",\"111160\",\"111276\",\"111241\"]," + "\n" +
+			"\t" + "\"data\":[[611,146,128,337]," + "\n" +
+					"\t" + "\t" + "[867,227,228,412]," + "\n" +
+					"\t" + "\t" + "[461,96,90,275]," + "\n" +
+					"\t" + "\t" + "[835,220,217,398]," + "\n" +
+					"\t" + "\t" + "[898,226,229,443]," + "\n" + 
+					"\t" + "\t" + "[454,77,125,252]," + "\n" +
+					"\t" + "\t" + "[579,130,111,338]," + "\n" +
+					"\t" + "\t" + "[973,236,237,500]" + "\n" +
+				"\t" + "]" + "\n" +
+		"}";
     }
-    
+	
     @Test
     public void testConvertCSVtoJSON() {
-        // You should test using the files in src/test/resources.
-        assertTrue(false);
+		String s = Converter.csvToJson(csvString);
+        assertEquals(jsonString, s);
     }
 
     @Test
     public void testConvertJSONtoCSV() {
-        // You should test using the files in src/test/resources.
-        assertTrue(false);
+		String s = Converter.jsonToCsv(jsonString);
+        assertEquals(csvString, s);
     }
 }
 
